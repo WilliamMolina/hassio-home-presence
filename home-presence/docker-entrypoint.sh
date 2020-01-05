5617ad1913404a261c3cb2af44d0eb4ffb88cccf
William Molina <wmolina98@gmail.com>
2020-01-04 21:41:45 -0500

    Updating info

--- home-presence/docker-entrypoint.sh
+++ home-presence/docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./presence -http_host_path=0.0.0.0:5555 -mqtt_client_id=happy_bubbles -mqtt_host=192.168.0.21:1883 -mqtt_password=password -mqtt_username=william
+./presence -http_host_path=0.0.0.0:5555 -mqtt_client_id=happy_bubbles -mqtt_host=192.168.0.7:1883 -mqtt_password=wondercodehomebroker -mqtt_username=wondercodehome
